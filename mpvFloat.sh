9d7e66bff998ddf3bb8f1713ddbaf7a26d1b1411
InigoGutierrez <inigogf.95@gmail.com>
2018-11-24 20:20:13 +0100

    mpv launchers now process all arguments instead of only first

--- mpvFloat.sh
+++ mpvFloat.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nohup mpv --geometry=-0-0 --autofit=40% --title="mpvfloat" "$1" >/dev/null &
+nohup mpv --geometry=-0-0 --autofit=40% --title="mpvfloat" "$@" >/dev/null &
--- mpvFloatLoop.sh
+++ mpvFloatLoop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nohup mpv --loop-playlist --geometry=-0-0 --autofit=40% --title="mpvfloat" "$1" >/dev/null &
+nohup mpv --loop-playlist --geometry=-0-0 --autofit=40% --title="mpvfloat" "$@" >/dev/null &
