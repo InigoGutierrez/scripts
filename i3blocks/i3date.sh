1dd90a8e2d6e23f7ad53aad941af6ce98ab2ee2c
InigoGutierrez <inigogf.95@gmail.com>
2019-01-01 23:39:39 +0100

    i3date doesn't show percentage.

--- i3blocks/i3date.sh
+++ i3blocks/i3date.sh
@@ -6,7 +6,7 @@ case $BLOCK_BUTTON in
 	3) ~/scripts/floats/floatYears.sh ;;
 esac
 
-dayofyear=$(date +%j)
-perc=$(echo "scale=1; ($dayofyear*100)/365" | bc)
+#dayofyear=$(date +%j)
+#perc=$(echo "scale=1; ($dayofyear*100)/365" | bc)
 #echo $(date +"%A, %y/%m/%d ($perc%) - %T")
-echo $(date +"%y/%m/%d ($perc%) - %H:%M")
+echo $(date +"%y/%m/%d - %H:%M")
