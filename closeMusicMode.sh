1bfac842cc193517c215a0fb5f9f129519d95a8a
InigoGutierrez <inigogf.95@gmail.com>
2019-03-20 16:25:12 +0100

    Dunstified and other minor changes.

--- closeMusicMode.sh
+++ closeMusicMode.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-kill $(pgrep -x musicMode.sh) # End the music mode process
+kill "$(pgrep -x "musicMode.sh")" # End the music mode process
 i3-msg "bar mode dock" # Restore the i3bar
-exec feh --bg-max ~/images/wallpapers/bg* # Restore default wallpaper
-notify-send -h string:x-canonical-private-synchronous:musicMode -t 1000 "Closed music mode."
+feh --bg-max ~/images/wallpapers/bg_* # Restore default wallpaper
+killall vis
+dunstify -r "$(dunstifyIDs.sh "musicMode")" -t 1000 "Closed music mode."
