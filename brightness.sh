b56400cb2572649a758714f0bd0d7b027b6a6fdd
InigoGutierrez <inigogf.95@gmail.com>
2019-03-20 16:18:58 +0100

    Dunstified and removed initial file permission change.

--- brightness.sh
+++ brightness.sh
@@ -3,27 +3,24 @@
 brightness_file=/sys/class/backlight/intel_backlight/brightness
 maxbrightness_file=/sys/class/backlight/intel_backlight/max_brightness
 
-permissions=$(ls -l $brightness_file | awk '{print $1}')
-if [ $permissions != "-rw-rw-rw-" ]; then
-	#urxvt -title floating -geometry 32x1 -e sudo ~/scripts/brightFirst.sh
-	sudo ~/scripts/brightFirst.sh
-fi
+# Old setup with initial file permissions change
+# permissions="$(ls -l $brightness_file | awk '{print $1}')"
+# if [ $permissions != "-rw-rw-rw-" ]; then
+# 	sudo ~/scripts/brightFirst.sh
+# fi
 
-brightness=$(< $brightness_file)
-maxbrightness=$(< $maxbrightness_file)
+brightness=$(< "$brightness_file")
+maxbrightness=$(< "$maxbrightness_file")
 let target=$1+$brightness
 if [ $target -gt $maxbrightness ]
 then
-	#echo "Value too big. Target: $target; max: $maxbrightness"
-	echo $maxbrightness | tee $brightness_file
+	sudo echo "$maxbrightness" >"$brightness_file"
 elif [ 0 -gt $target ]
 then
-	#echo "Value too low. Target: $target"
-	echo "0" | tee $brightness_file
+	sudo echo "0" >"$brightness_file"
 else
-	echo $target | tee $brightness_file
-	#echo "New brightness: $target/$maxbrightness"
+	sudo echo "$target" >"$brightness_file"
 fi
 
-notify-send -h string:x-canonical-private-synchronous:vol -t 500 "$(~/scripts/i3blocks/i3brightness.sh)"
+dunstify -r $(dunstifyIDs.sh "brightness") -t 500 "$(~/scripts/i3blocks/i3brightness.sh)"
 pkill -SIGRTMIN+11 i3blocks
