cb328308bc7375de7f8b695761e56d237a9e1304
InigoGutierrez <inigogf.95@gmail.com>
2019-03-20 16:31:14 +0100

    Created showURLimage.sh

---
+++ showURLimage.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Downloads an url to /tmp and opens it with sxiv.
+
+filename=/tmp/showURLimage$(date +%H%M%S)
+curl $1 > $filename
+sxiv -b -N sxivfloat $filename || notify-send "No image found at $1"
