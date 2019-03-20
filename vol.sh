186d6435c9a5b5831dd1b1a522107cb96b2910e5
InigoGutierrez <inigogf.95@gmail.com>
2019-03-20 16:29:09 +0100

    Dunstified and minor changes.

--- vol.sh
+++ vol.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-sink=$(pactl list sinks | grep "^Sink " | cut -d'#' -f2)
+sink="$(pactl list sinks | grep "^Sink " | cut -d'#' -f2)"
 if [ $1 = "mute" ]; then
-	pactl set-sink-mute $sink toggle
+	pactl set-sink-mute "$sink" toggle
 else
-	pactl set-sink-volume $sink $1%
+	pactl set-sink-volume "$sink" $1%
 fi
 
-notify-send -h string:x-canonical-private-synchronous:vol -t 500 "$(~/scripts/i3blocks/i3vol.sh)"
+dunstify -r $(dunstifyIDs.sh "vol") -t 500 "$(~/scripts/i3blocks/i3vol.sh)"
 pkill -SIGRTMIN+10 i3blocks
