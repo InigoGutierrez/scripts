b2bdbb94e5171133853e80de6af5b6605477ce4c
InigoGutierrez <inigogf.95@gmail.com>
2019-04-20 15:27:06 +0200

    Added Spanish dictionary.

--- dmenuDic.sh
+++ dmenuDic.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-# Selects a word from the system's dictionary with the help of dmenu and copies it to clipboard.
+# Selects a word from a dictionary with the help of dmenu and copies it to clipboard.
 
-word="$(dmenu -i -l 10 -p "Choose a word:" < /usr/share/dict/cracklib-small)"
+dict="$(printf "EN\nES" | dmenu -i -p "Choose a dictionary:")"
+if [ "$dict" = "EN" ]; then
+	word="$(dmenu -i -l 10 -p "Choose a word:" < /usr/share/dict/words.txt)"
+elif [ "$dict" = "ES" ]; then
+	word="$(dmenu -i -l 10 -p "Elige una palabra:" < /usr/share/dict/palabras.txt)"
+fi
 [ "$word" ] || exit 0
 echo "$word" | xsel -bi
 dunstify -t 2000 "Written [$word] into clipboard."
--- fzfdic.sh
+++ fzfdic.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-cat /usr/share/dict/cracklib-small | fzf -e
+dict="$(printf "EN\nES" | fzf -e --prompt="Choose a dictionary>")"
+if [ "$dict" = "EN" ]; then
+	fzf -e --prompt="EN" < /usr/share/dict/words.txt
+elif [ "$dict" = "ES" ]; then
+	fzf -e --prompt="ES" < /usr/share/dict/palabras.txt
+fi
