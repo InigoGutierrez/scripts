a02defa8b7c90214c5898e98317e14473a246a61
InigoGutierrez <inigogf.95@gmail.com>
2019-05-09 11:29:31 +0200

    Added Polish dictionary.

--- dmenuDic.sh
+++ dmenuDic.sh
@@ -2,11 +2,13 @@
 
 # Selects a word from a dictionary with the help of dmenu and copies it to clipboard.
 
-dict="$(printf "EN\nES" | dmenu -i -p "Choose a dictionary:")"
+dict="$(printf "EN\nES\nPL" | dmenu -i -p "Choose a dictionary:")"
 if [ "$dict" = "EN" ]; then
 	word="$(dmenu -i -l 10 -p "Choose a word:" < /usr/share/dict/words.txt)"
 elif [ "$dict" = "ES" ]; then
 	word="$(dmenu -i -l 10 -p "Elige una palabra:" < /usr/share/dict/palabras.txt)"
+elif [ "$dict" = "PL" ]; then
+	word="$(dmenu -i -l 10 -p "Wybierz słowo:" < /usr/share/dict/słowa.txt)"
 fi
 [ "$word" ] || exit 0
 echo "$word" | tr -d '\n' | xsel -bi
