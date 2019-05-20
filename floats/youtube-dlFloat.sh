1c080783990429918a4b7fa4d383b36bf63fae1f
InigoGutierrez <inigogf.95@gmail.com>
2019-05-20 23:43:56 +0200

    Now using $HOME and video is now downloaded with metadata.

--- floats/youtube-dlFloat.sh
+++ floats/youtube-dlFloat.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-urxvt -title "floating" -geometry "100x8-0-0" -e youtube-dl --all-subs --embed-subs -o "~/downloads/videos/%(title)s.%(ext)s" "$1" &
+urxvt -title "floating" -geometry "100x8-0-0" -e youtube-dl --add-metadata --all-subs --embed-subs -o "$HOME/downloads/videos/%(title)s.%(ext)s" "$1"
