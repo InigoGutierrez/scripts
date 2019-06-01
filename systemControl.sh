73b087ef662445cd4dc2f3addcd2e1bb3b7d27fb
InigoGutierrez <inigogf.95@gmail.com>
2019-06-01 18:18:40 +0200

    Added option to hybernate.

--- systemControl.sh
+++ systemControl.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # A dmenu prompt script to control system functions.
 
-options="lock\nreboot\nshutdown\nexit i3"
+options="lock\nreboot\nshutdown\nhibernate\nexit i3"
 
 chosen=$(echo -e "$options" | dmenu -p ">_")
 
 case "$chosen" in
 	lock) i3lock.sh ;;
-	reboot) ~/scripts/prompt.sh Reboot? reboot ;;
-	shutdown) ~/scripts/prompt.sh Shutdown? "shutdown now" ;;
+	reboot) ~/scripts/prompt.sh Reboot? "systemctl reboot" ;;
+	shutdown) ~/scripts/prompt.sh Shutdown? "systemctl poweroff" ;;
+	hibernate) ~/scripts/prompt.sh Hibernate? "systemctl hibernate" ;;
 	"exit i3") ~/scripts/prompt.sh "Exit i3? (ends X session)" "i3-msg exit" ;;
 esac
