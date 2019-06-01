37ff1b11113179a762f76d148e64eea66192c1d6
InigoGutierrez <inigogf.95@gmail.com>
2019-06-01 18:18:50 +0200

    Changed lock effect.

--- i3lock.sh
+++ i3lock.sh
@@ -10,5 +10,6 @@ maim "$imgTemp"
 #[ -e "$imgLock" ] && convert "$imgTemp" "$imgLock" -gravity center -composite -monochrome "$imgFinal"
 #[ -e "$imgLock" ] && convert "$imgTemp" -edge 3 "$imgLock" -gravity center -composite "$imgFinal"
 #[ -e "$imgLock" ] && convert "$imgTemp" -colors 8 "$imgFinal"
-[ -e "$imgLock" ] && convert "$imgTemp" -canny 1x1 "$imgFinal"
+[ -e "$imgLock" ] && convert "$imgTemp" -canny 1x1 +level-colors "#093145","#e0ea99" "$imgFinal"
+identify -verbose "$imgFinal" >~/identify.log
 i3lock -i "$imgFinal" -f || i3lock -i "$imgTemp" -f
