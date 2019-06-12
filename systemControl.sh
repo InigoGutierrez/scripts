92302eab66110fd7b8f11795241accdf7ec89154
InigoGutierrez <inigogf.95@gmail.com>
2019-06-12 14:26:04 +0200

    Added option to suspend.

--- systemControl.sh
+++ systemControl.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # A dmenu prompt script to control system functions.
 
-options="lock\nreboot\nshutdown\nhibernate\nexit i3"
+options="lock\nreboot\nshutdown\nsuspend\nhibernate\nexit i3"
 
 chosen=$(echo -e "$options" | dmenu -p ">_")
 
 case "$chosen" in
-	lock) i3lock.sh ;;
-	reboot) ~/scripts/prompt.sh Reboot? "systemctl reboot" ;;
-	shutdown) ~/scripts/prompt.sh Shutdown? "systemctl poweroff" ;;
-	hibernate) ~/scripts/prompt.sh Hibernate? "systemctl hibernate" ;;
+	"lock") i3lock.sh ;;
+	"reboot") ~/scripts/prompt.sh Reboot? "systemctl reboot" ;;
+	"shutdown") ~/scripts/prompt.sh Shutdown? "systemctl poweroff" ;;
+	"suspend") systemctl suspend ;;
+	"hibernate") ~/scripts/prompt.sh Hibernate? "systemctl hibernate" ;;
 	"exit i3") ~/scripts/prompt.sh "Exit i3? (ends X session)" "i3-msg exit" ;;
 esac
