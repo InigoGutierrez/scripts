afb1c135fbee2c1c9966293b63ea5b73be80fffe
InigoGutierrez <inigogf.95@gmail.com>
2019-06-12 14:25:31 +0200

    Changed lock effect.

--- i3lock.sh
+++ i3lock.sh
@@ -8,8 +8,7 @@ imgFinal="/tmp/lockFinal$(date +"%H%M%S").png"
 maim "$imgTemp"
 #[ -e "$imgLock" ] && convert "$imgTemp" -paint 1 -swirl -30 "$imgLock" -gravity center -composite "$imgFinal"
 #[ -e "$imgLock" ] && convert "$imgTemp" "$imgLock" -gravity center -composite -monochrome "$imgFinal"
-#[ -e "$imgLock" ] && convert "$imgTemp" -edge 3 "$imgLock" -gravity center -composite "$imgFinal"
-#[ -e "$imgLock" ] && convert "$imgTemp" -colors 8 "$imgFinal"
-[ -e "$imgLock" ] && convert "$imgTemp" -canny 1x1 +level-colors "#093145","#e0ea99" "$imgFinal"
-identify -verbose "$imgFinal" >~/identify.log
+#[ -e "$imgLock" ] && convert "$imgTemp" -canny 1x1 +level-colors "#093145","#e0ea99" "$imgFinal"
+#[ -e "$imgLock" ] && convert "$imgTemp" -type Grayscale -paint 1 "$imgFinal"
+[ -e "$imgLock" ] && convert "$imgTemp" -colors 2 +level-colors "#00001f","000000" "$imgFinal"
 i3lock -i "$imgFinal" -f || i3lock -i "$imgTemp" -f
