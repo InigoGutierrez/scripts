0cb59b46e890a86e67133fdc561671fa5b371d6f
InigoGutierrez <inigogf.95@gmail.com>
2019-10-15 20:35:37 +0200

    Added dunstifyID and changed some emojis on i3moon.sh.

--- i3blocks/i3moon.sh
+++ i3blocks/i3moon.sh
@@ -2,18 +2,19 @@
 
 # Shows an emoji with the phase of the moon
 
-pom | grep -q "New Moon" && moon=🌑
+[ ! -f "/bin/pom" ] && echo "🌜 bsd-games not installed. 🌛" && exit 0
+pom | grep -q "New" && moon=🌚
 pom | grep -q "Waxing Crescent" && moon=🌒
 pom | grep -q "First Quarter" && moon=🌓
 pom | grep -q "Waxing Gibbous" && moon=🌔
-pom | grep -q "Full moon" && moon=🌕
+pom | grep -q "Full" && moon=🌝
 pom | grep -q "Waning Gibbous" && moon=🌖
 pom | grep -q "Last Quarter" && moon=🌗
 pom | grep -q "Waning Crescent" && moon=🌘
 
-case $BLOCK_BUTTON in
-	1) dunstify "$moon $(pom) $moon";;
-esac
-
 echo "$moon"
+
+case $BLOCK_BUTTON in
+	1) dunstify -r "$(dunstifyIDs.sh "moon")" "$moon $(pom) $moon";;
+esac
 
