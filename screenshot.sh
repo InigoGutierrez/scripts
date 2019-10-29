8a53c22b644cb74186e2830e7f34113468a145d5
InigoGutierrez <inigogf.95@gmail.com>
2019-10-29 19:51:12 +0100

    Added option to paste to clipboard for screnshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -2,7 +2,10 @@
 
 # Takes a screenshot
 
+borderColor="0.412,0.431,1"
 name="$(date +"%y%m%d_%H%M%S")".png
 file="$HOME/images/screenshots/""$name"
-[ "$1" = "" ] && maim -m 8 "$file" && dunstify -t 800 "Screenshot $name taken."
-[ "$1" = "-a" ] && maim -s -b 2 -c 0.412,0.431,1 -m 8 "$file" && dunstify -t 800 "Screenshot $name taken."
+
+[ "$1" = "" ] && maim -m 8 "$file" && notify-send -t 800 "Screenshot $name taken."
+[ "$1" = "-a" ] && maim -s -b 2 -c "$borderColor" -m 8 "$file" && notify-send -t 800 "Screenshot $name taken."
+[ "$1" = "-c" ] && maim -s -b 2 -c "$borderColor" -m 8 | xclip -selection clipboard -t image/png && notify-send -t 800 "Screenshot copied to clipboard."
