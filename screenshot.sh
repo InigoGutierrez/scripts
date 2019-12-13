3bf42f723a523735b2f5d9c7ba81d9bc0f88ea2d
InigoGutierrez <inigogf.95@gmail.com>
2019-12-13 14:42:20 +0100

    Now screenshots don't show the cursor.

--- screenshot.sh
+++ screenshot.sh
@@ -6,6 +6,6 @@ borderColor="0.412,0.431,1"
 name="$(date +"%y%m%d_%H%M%S")".png
 file="$HOME/images/screenshots/""$name"
 
-[ "$1" = "" ] && maim -m 8 "$file" && notify-send -t 800 "Screenshot $name taken."
-[ "$1" = "-a" ] && maim -s -b 2 -c "$borderColor" -m 8 "$file" && notify-send -t 800 "Screenshot $name taken."
-[ "$1" = "-c" ] && maim -s -b 2 -c "$borderColor" -m 8 | xclip -selection clipboard -t image/png && notify-send -t 800 "Screenshot copied to clipboard."
+[ "$1" = "" ] && maim -u -m 8 "$file" && notify-send -t 800 "Screenshot $name taken."
+[ "$1" = "-a" ] && maim -u -s -b 2 -c "$borderColor" -m 8 "$file" && notify-send -t 800 "Screenshot $name taken."
+[ "$1" = "-c" ] && maim -u -s -b 2 -c "$borderColor" -m 8 | xclip -selection clipboard -t image/png && notify-send -t 800 "Screenshot copied to clipboard."
