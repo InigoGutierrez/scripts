efd23aceb4d6fabe3228d7c217a62be2e5b5bf87
InigoGutierrez <inigogf.95@gmail.com>
2019-12-18 19:28:09 +0100

    screenRecord.sh creates target folder if it does not exist.

--- screenRecord.sh
+++ screenRecord.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 MARKFILE='/tmp/screenRec.mark'
+recsFolder="$HOME/videos/recs"
 # Check if already recording
 if [ -e $MARKFILE ]; then
 	stopScreenRecord.sh
 else
 	touch $MARKFILE
-	filename=rec"$(date +"%y-%m-%d_%H-%M-%S")".mp4
+	mkdir -p "$recsFolder"
+	filename=rec"$(date +"%y-%m-%d_%H-%M-%S")".mkv
 	# also record microphone
 	# ffmpeg -video_size "$(getResolution.sh)" -framerate 30 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i hw:0,0 "$HOME/videos/recs/$filename" &
-	ffmpeg -video_size "$(getResolution.sh)" -framerate 30 -f x11grab -i :0.0+0,0 "$HOME/videos/recs/$filename" &
+	ffmpeg -video_size "$(getResolution.sh)" -framerate 30 -f x11grab -i :0.0 -vcodec libx264 "$recsFolder/$filename" &
 	notify-send -t 1000 "Recording $filename"
 	pkill -SIGRTMIN+13 i3blocks
 fi
