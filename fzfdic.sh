384a2b36c86be5e79913a1861d8eb23ec3bf65cb
InigoGutierrez <inigogf.95@gmail.com>
2020-03-04 23:18:24 +0100

    fzfdic.sh now uses global configuration for dictionary files instead of explicit paths.

--- fzfdic.sh
+++ fzfdic.sh
@@ -2,7 +2,7 @@
 
 dict="$(printf "EN\nES" | fzf -e -i --prompt="Choose a dictionary>")"
 if [ "$dict" = "EN" ]; then
-	fzf -e -i --prompt="EN>" < /usr/share/dict/words.txt
+	fzf -x -i --prompt="EN>" < "$CONFIG_DIC_EN"
 elif [ "$dict" = "ES" ]; then
-	fzf -e -i --prompt="ES>" < /usr/share/dict/palabras.txt
+	fzf -x -i --prompt="ES>" < "$CONFIG_DIC_ES"
 fi
