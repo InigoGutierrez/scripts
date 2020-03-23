e89978b6c45c6229c6341c1dbf9c28b7ba254a77
InigoGutierrez <inigogf.95@gmail.com>
2020-03-23 23:50:05 +0100

    Coronavirus Spain daily status script for i3blocks.

---
+++ i3blocks/i3corona.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# i3corona.sh
+#
+# Shows daily stats about coronavirus in Spain
+
+#case $BLOCK_BUTTON in
+#	1) # Left click
+#	2) # Middle click
+#	3) # Right click
+#esac
+
+tmpDir="/tmp/corona"
+tmpFile="${tmpDir}/coronaData.txt"
+
+[ -d "$tmpDir" ] || mkdir -p "$tmpDir"
+[ -f "$tmpFile" ] ||
+	curl https://corona-stats.online/spain > "$tmpFile" 2>/dev/null
+
+grep 'Spain' "$tmpFile" | sed 's/\s*//g; s/\x1b\[[0-9;]*m//g; s/[,▲]//g' |
+	awk -F'│' '{print "😷 " $3 " (+" $9 ") 💪 " $8 "%"}'
+
