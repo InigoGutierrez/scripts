f166f9d2afbc9bd39a58db806fcc659c787f7455
InigoGutierrez <inigogf.95@gmail.com>
2020-05-07 23:40:35 +0200

    Fixed showURLimage.sh to detect and show thumbnails of youtube videos.

--- showURLimage.sh
+++ showURLimage.sh
@@ -3,11 +3,14 @@
 # Downloads an url to /tmp and opens it with sxiv.
 # If the url is from youtube tries to download the thumbnail.
 
-url="$1"
-echo "$url" | grep -i "youtube" >/dev/null &&
-	url="$(youtube-dl --get-thumbnail "$url")"
-
 folder="/tmp/showURLimage"
+
+url="$1"
+if echo "$url" | grep -i "youtube" >/dev/null; then
+	url="$(youtube-dl --get-thumbnail "$url")"
+	folder="${folder}/$(date +%H%M%S)"
+fi
+
 filename="${folder}/$(date +%H%M%S)"
 
 [ -d "$folder" ] || mkdir -p "$folder"
