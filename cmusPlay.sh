342678d45fb132d0a08f315123a468c4bfbc2731
InigoGutierrez <inigogf.95@gmail.com>
2020-05-07 23:50:01 +0200

    Reshaped cmus terminal.

--- cmusPlay.sh
+++ cmusPlay.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+tmux=""
 [ -f "/usr/bin/tmux" ] && tmux="tmux new-session -s cmus"
 if ! pgrep -x cmus ; then
-	urxvt -title floating -sh 20 -geometry "185x15+30-28" -cd ~/music -e $tmux cmus
+	urxvt -title floating -geometry "159x15+44+450" -cd ~/music -e $tmux cmus
 else
 	cmus-remote -u
 fi
 
 pkill -SIGRTMIN+12 i3blocks
+
