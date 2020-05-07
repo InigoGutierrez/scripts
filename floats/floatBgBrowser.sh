4919a7bceea7ce4975fbd9d85c3d4a34697e7b6c
InigoGutierrez <inigogf.95@gmail.com>
2020-05-08 00:14:08 +0200

    floatBGBrowser.sh has now a default folder.

--- floats/floatBgBrowser.sh
+++ floats/floatBgBrowser.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-urxvt -title floating -sh 20 -cr black -geometry "45x6-5-5" -e ~/scripts/bgBrowser.sh "$1"
+path="$1"
+[ -z "$path" ] && path="/home/taamas/images/wallpapers/"
+urxvt -title floating -sh 20 -cr black -geometry "45x6-5-5" -e ~/scripts/bgBrowser.sh "$path"
