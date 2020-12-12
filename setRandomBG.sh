8b1f6ba1f5c8df81646fb64597026747ba4d3d87
InigoGutierrez <inigogf.95@gmail.com>
2020-12-12 14:34:50 +0100

    Now setRandomBG.sh creates a softlink to the wallpaper at ~.

--- setRandomBG.sh
+++ setRandomBG.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-# Sets a random image as wallpaper.
+# Sets a random image as wallpaper and soft-links it from home folder.
 
 bgsFolder="$CONFIG_FOLDER_RANDOMBG"
 [ -z "$bgsFolder" ] && dunstify '$CONFIG_FOLDER_RANDOMBG env variable not set.' && exit 0
 [ ! -d "$bgsFolder" ] && dunstify "'$CONFIG_FOLDER_RANDOMBG' folder does not exist." && exit 0
-feh --bg-max "$(ls -d "$CONFIG_FOLDER_RANDOMBG"/* | shuf -n 1)"
+wpFile="$(ls -d "$CONFIG_FOLDER_RANDOMBG"/* | shuf -n 1)"
+feh --bg-max "$wpFile"
+ln -sf "$wpFile" "${HOME}/wp"
