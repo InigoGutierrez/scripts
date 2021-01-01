03aa215fd6bf9476f2d7c57508e7f0d7b4b37bf4
InigoGutierrez <inigogf.95@gmail.com>
2021-01-01 18:48:52 +0100

    Better readability.

--- surfraw.sh
+++ surfraw.sh
@@ -3,9 +3,16 @@
 # Do stuff using surfraw. Take back internet browsing from evil.
 
 search=$(echo "" | dmenu -p "Search:")
-[ "$search" ] || exit 0
+[ -n "$search" ] || exit 0
 dunstify "Searching $search"
-target="$(ddgr --json "$search" | grep -E '"title": |"url": ' | sed '/"title": / N;s/\n//g' | awk '-F ["]' '{print $3 "[" $5 "]" }' | sed 's/"//g;s/,\s*\[/  \[/' | dmenu -i -l 10 -p "Target:" | awk '-F [[]' '{print $2}' | sed 's/]\s*$//')"
+target="$(ddgr --json "$search" |
+	grep -E '"title": |"url": ' |
+	sed '/"title": / N;s/\n//g' |
+	awk '-F ["]' '{print $3 "[" $5 "]" }' |
+	sed 's/"//g;s/,\s*\[/  \[/' |
+	dmenu -i -l 10 -p "Target:" |
+	awk '-F [[]' '{print $2}' |
+	sed 's/]\s*$//')"
 dunstify "Trying to play result of $target"
 dunstify "Url: $target"
 mpv "$target" || qutebrowser "$target" || dunstify "URL not valid."
