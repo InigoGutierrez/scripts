60ecffe5784e5980939416dcbf22b81a26a5de62
InigoGutierrez <inigogf.95@gmail.com>
2021-01-01 18:49:28 +0100

    Better image showing.

--- showURLimage.sh
+++ showURLimage.sh
@@ -4,16 +4,18 @@
 # If the url is from youtube tries to download the thumbnail.
 
 folder="/tmp/showURLimage"
-
-url="$1"
-if echo "$url" | grep -i "youtube" >/dev/null; then
-	url="$(youtube-dl --get-thumbnail "$url")"
-	folder="${folder}/$(date +%H%M%S)"
-fi
-
 filename="${folder}/$(date +%H%M%S)"
 
-[ -d "$folder" ] || mkdir -p "$folder"
-curl "$url" > "$filename"
-sxiv -b -N "sxivfloat" "$filename" || notify-send "No image found at $url"
+url="$1"
 
+[ -d "$folder" ] || mkdir -p "$folder"
+
+if echo "$url" | grep -i "youtube" >/dev/null; then
+	youtube-dl --skip-download --write-thumbnail --no-playlist -o "$filename" "$url"
+	convert "${filename}*" "${filename}.jpg"
+	filename="${filename}.jpg"
+else
+	curl "$url" > "$filename"
+fi
+
+sxiv -b -N "sxivfloat" "$filename" || notify-send "No image found at $url"
